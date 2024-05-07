7993329de04a2ac3271bcfea555a98cc9708d376
park <uyeongi@naver.com>
2024-05-07 16:13:14 +0900

    aa

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views 
 from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,TokenBlacklistView,)
-from .views import LoginView, LogoutView
+
 
 app_name = "accounts"
 
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer, UserDetailSerializer
 from .models import PasswordQuestion
