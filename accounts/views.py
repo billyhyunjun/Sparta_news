fa63e0bb526a0c01d09e5604d1e23a12a20e06e9
park <uyeongi@naver.com>
2024-05-07 15:44:24 +0900

    회원가입

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
