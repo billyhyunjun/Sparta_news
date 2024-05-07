1aff10d7d3a6215defaf76d8ae65eb5a00108d9d
Billyhyunjun <phjphj9999@gmail.com>
2024-05-07 10:41:53 +0900

    오류 수정 - 로그인, 비밀번호 변경, 암호문 생성

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,7 +9,17 @@ class UserSerializer(serializers.ModelSerializer):
             "id",
             "username",
             "email",
+            "password",
+            "password_question",
+            "password_answer",
         ]
+        
+    def create(self, validated_data):
+        # 받아온 비밀번호를 해시로 변환
+        hashed_password = make_password(validated_data['password'])
+        # 변환된 해시를 validated_data에 업데이트
+        validated_data['password'] = hashed_password
+        return super().create(validated_data)
 
     def validate(self, attrs):
         attrs = super().validate(attrs)
@@ -16,5 +27,22 @@ class UserSerializer(serializers.ModelSerializer):
         if "username" in attrs:
             if get_user_model().objects.filter(username=attrs["username"]).exists():
                 raise serializers.ValidationError("username exists")
+        if "email" in attrs:
+            if get_user_model().objects.filter(email=attrs["email"]).exists():
+                raise serializers.ValidationError("email exists")
+        if not "password_question" in attrs:
+            raise serializers.ValidationError("password_question is required ")
+        if not "password_answer" in attrs:
+            raise serializers.ValidationError("password_answer is required")
 
         return attrs
+    
+    
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = [
+            "username",
+            "email",
+        ]
+
