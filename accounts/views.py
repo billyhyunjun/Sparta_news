a05a76f90de6bbeeaf8382827b1a3b1d0f3cce59
park <uyeongi@naver.com>
2024-05-07 15:43:07 +0900

    회원가입

--- accounts/views.py
+++ accounts/views.py
@@ -28,37 +28,6 @@ class AccountAPIView(APIView):
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            
-            
-            refresh_token = RefreshToken.for_user(user)
-            data = {
-                'user_id': user.id,
-                'access_token': str(refresh_token.access_token),
-                'refresh_token': str(refresh_token)
-            }
-            
-            return Response(data, status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "아이디 및 비밀번호가 이상한데요a?"}, status=status.HTTP_400_BAD_REQUEST)
-
-class LogoutView(APIView):
-    def post(self, request):
-        refresh_token = request.data.get('refresh_token')
-        if refresh_token:
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            logout(request)
-            return Response({"message": "로그아웃 인가?"}, status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "리프레시 토큰이 필요합니다."}, status=status.HTTP_400_BAD_REQUEST)
-
 class AccountDetailAPIView(APIView):
     # 로그인상태
     permission_classes = [IsAuthenticated]
