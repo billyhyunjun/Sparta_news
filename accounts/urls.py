8aba7eb9a11dc3905665d8e84cfe9dc7aae20e9d
park <uyeongi@naver.com>
2024-05-07 15:43:46 +0900

    회원가입

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,6 @@ app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.AccountAPIView.as_view(), name="signup"),
-    path('login/',LoginView.as_view(), name='user_login'),
-    path('logout/', LogoutView.as_view(), name='user_logout'),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("logout/", TokenBlacklistView.as_view(), name="logout"),
